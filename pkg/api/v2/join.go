2147807cabe4b53b0e39f15fd1f3f194b8a72665
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-03-24 18:26:54 +0200

    Implement v2/join

---
+++ pkg/api/v2/join.go
@@ -0,0 +1,204 @@
+package v2
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net"
+	"strings"
+
+	"github.com/canonical/microk8s-cluster-agent/pkg/util"
+)
+
+// WorkerOnlyField is the "worker" field of the JoinRequest message.
+type WorkerOnlyField bool
+
+// UnmarshalJSON implements json.Unmarshaler.
+// It handles boolean values, as well as the string value "as-worker".
+func (v *WorkerOnlyField) UnmarshalJSON(b []byte) error {
+	*v = WorkerOnlyField(bytes.Equal(b, []byte("true")) || bytes.Equal(b, []byte(`"as-worker"`)))
+	return nil
+}
+
+// JoinRequest is the request message for the v2/join API endpoint.
+type JoinRequest struct {
+	// ClusterToken is the token generated during "microk8s add-node".
+	ClusterToken string `json:"token"`
+	// RemoteHostName is the hostname of the joining host.
+	RemoteHostName string `json:"hostname"`
+	// ClusterAgentPort is the port number where the cluster-agent is listening on the joining node.
+	ClusterAgentPort string `json:"port"`
+	// WorkerOnly is true when joining a worker-only node.
+	WorkerOnly WorkerOnlyField `json:"worker"`
+	// HostPort is the hostname and port that accepted the request. This is retrieved directly from the *http.Request object.
+	HostPort string `json:"-"`
+	// RemoteAddress is the remote address from which the join request originates. This is retrieved directly from the *http.Request object.
+	RemoteAddress string `json:"-"`
+}
+
+// JoinResponse is the response message for the v2/join API endpoint.
+type JoinResponse struct {
+	// CertificateAuthority is the root CertificateAuthority certificate for the Kubernetes cluster.
+	CertificateAuthority string `json:"ca"`
+	// CallbackToken is a callback token used to authenticate requests with the cluster agent on the joining node.
+	CallbackToken string `json:"callback_token"`
+	// APIServerPort is the port where the kube-apiserver is listening.
+	APIServerPort string `json:"apiport"`
+	// APIServerAuthorizationMode is the AuthorizationMode used by kube-apiserver.
+	APIServerAuthorizationMode string `json:"api_authz_mode"`
+	// KubeletArgs is a string with arguments for the kubelet service on the joining node.
+	KubeletArgs string `json:"kubelet_args"`
+	// HostNameOverride is the host name the joining node will be known as in the MicroK8s cluster.
+	HostNameOverride string `json:"hostname_override"`
+	// DqliteVoterNodes is a list of known dqlite voter nodes. Each voter is identified as "$IP_ADDRESS:$PORT".
+	// This is not included in the response when joining worker-only nodes.
+	DqliteVoterNodes []string `json:"voters,omitempty"`
+	// ServiceAccountKey is the private key used for signing ServiceAccount tokens.
+	// This is not included in the response when joining worker-only nodes.
+	ServiceAccountKey string `json:"service_account_key"`
+	// AdminToken is a static token used to authenticate in the MicroK8s cluster as "admin".
+	// This is not included in the response when joining worker-only nodes.
+	AdminToken string `json:"admin_token,omitempty"`
+	// CertificateAuthorityKey is the private key of the Certificate Authority.
+	// Note that this is defined as *string, since the Python code expects this to be explicitly None/nil/null for worker only nodes.
+	// This is not included in the response when joining worker-only nodes.
+	CertificateAuthorityKey *string `json:"ca_key"`
+	// DqliteClusterCertificate is the certificate for connecting to the Dqlite cluster.
+	// This is not included in the response when joining worker-only nodes.
+	DqliteClusterCertificate string `json:"cluster_cert,omitempty"`
+	// DqliteClusterKey is the key for connecting to the Dqlite cluster.
+	// This is not included in the response when joining worker-only nodes.
+	DqliteClusterKey string `json:"cluster_key,omitempty"`
+	// ControlPlaneNodes is a list of known control plane nodes running kube-apiserver.
+	// This is only included in the response when joining worker-only nodes.
+	ControlPlaneNodes []string `json:"control_plane_nodes"`
+}
+
+// Join implements "POST v2/join".
+func (a *API) Join(ctx context.Context, req JoinRequest) (*JoinResponse, error) {
+	if !util.IsValidClusterToken(req.ClusterToken) {
+		return nil, fmt.Errorf("invalid cluster token")
+	}
+	if err := util.RemoveClusterToken(req.ClusterToken); err != nil {
+		return nil, fmt.Errorf("failed to remove cluster token: %w", err)
+	}
+	if !util.HasDqliteLock() {
+		return nil, fmt.Errorf("failed to join the cluster: this is not an HA MicroK8s cluster")
+	}
+
+	// Sanity check cluster agent ports.
+	clusterAgentBind := util.GetServiceArgument("cluster-agent", "--bind")
+	_, port, _ := net.SplitHostPort(clusterAgentBind)
+	if port != req.ClusterAgentPort {
+		return nil, fmt.Errorf("cluster agent port needs to be set to %s", port)
+	}
+
+	// Sanity check node is not in cluster already.
+	remoteIP, _, _ := net.SplitHostPort(req.RemoteAddress)
+	dqliteCluster, err := util.GetDqliteCluster()
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve dqlite cluster nodes: %w", err)
+	}
+	for _, node := range dqliteCluster {
+		if strings.HasPrefix(node.Address, remoteIP+":") {
+			return nil, fmt.Errorf("joining node %q is already known to dqlite", remoteIP)
+		}
+	}
+
+	// TODO(akolaitis): Sanity check that hostname resolves to expected IP address.
+
+	// Update dqlite cluster if needed
+	if len(dqliteCluster) == 1 && strings.HasPrefix(dqliteCluster[0].Address, "127.0.0.1:") {
+		requestHost, _, _ := net.SplitHostPort(req.HostPort)
+		if err := util.UpdateDqliteIP(ctx, requestHost); err != nil {
+			return nil, fmt.Errorf("failed to update dqlite address to %q: %w", requestHost, err)
+		}
+
+		// Wait for dqlite cluster to come up with new address
+		dqliteCluster, err = util.WaitForDqliteCluster(ctx, func(c util.DqliteCluster) (bool, error) {
+			return len(c) >= 1 && !strings.HasPrefix(c[0].Address, "127.0.0.1:"), nil
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed waiting for dqlite cluster to come up: %w", err)
+		}
+	}
+
+	callbackToken, err := util.GetOrCreateSelfCallbackToken()
+	if err != nil {
+		return nil, fmt.Errorf("could not retrieve self callback token: %w", err)
+	}
+
+	ca, err := util.ReadFile(util.SnapDataPath("certs", "ca.crt"))
+	if err != nil {
+		return nil, fmt.Errorf("failed reading cluster CA: %w", err)
+	}
+	kubeletArgs, err := util.ReadFile(util.SnapDataPath("args", "kubelet"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to read arguments of kubelet service: %w", err)
+	}
+	if util.GetRemoteHost(req.RemoteHostName, req.RemoteAddress) != req.RemoteHostName {
+		kubeletArgs = fmt.Sprintf("%s\n--hostname-override=%s", kubeletArgs, remoteIP)
+	}
+
+	if err := util.MaybePatchCalicoAutoDetectionMethod(ctx, remoteIP, true); err != nil {
+		return nil, fmt.Errorf("failed to update cni configuration: %w", err)
+	}
+
+	if err := util.CreateNoCertsReissueLock(); err != nil {
+		return nil, fmt.Errorf("failed to create lock file to disable certificate reissuing: %w", err)
+	}
+	response := &JoinResponse{
+		CertificateAuthority:       ca,
+		CallbackToken:              callbackToken,
+		APIServerPort:              util.GetServiceArgument("kube-apiserver", "--secure-port"),
+		APIServerAuthorizationMode: util.GetServiceArgument("kube-apiserver", "--authorization-mode"),
+		HostNameOverride:           remoteIP,
+		KubeletArgs:                kubeletArgs,
+	}
+
+	if req.WorkerOnly {
+		if err := util.AddCertificateRequestToken(fmt.Sprintf("%s-kubelet", req.ClusterToken)); err != nil {
+			return nil, fmt.Errorf("failed adding certificate request token for kubelet: %w", err)
+		}
+		if err := util.AddCertificateRequestToken(fmt.Sprintf("%s-proxy", req.ClusterToken)); err != nil {
+			return nil, fmt.Errorf("failed adding certificate request token for kube-proxy: %w", err)
+		}
+
+		controlPlaneNodes, err := a.ListControlPlaneNodeIPs(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve list of control plane nodes: %w", err)
+		}
+		response.ControlPlaneNodes = controlPlaneNodes
+	} else {
+		caKey, err := util.ReadFile(util.SnapDataPath("certs", "ca.key"))
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve cluster CA key: %w", err)
+		}
+		response.CertificateAuthorityKey = &caKey
+		response.ServiceAccountKey, err = util.ReadFile(util.SnapDataPath("certs", "serviceaccount.key"))
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve service account key: %w", err)
+		}
+		response.AdminToken, err = util.GetKnownToken("admin")
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve token for admin user: %w", err)
+		}
+		response.DqliteClusterCertificate, err = util.ReadFile(util.SnapDataPath("var", "kubernetes", "backend", "cluster.crt"))
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve dqlite cluster certificate: %w", err)
+		}
+		response.DqliteClusterKey, err = util.ReadFile(util.SnapDataPath("var", "kubernetes", "backend", "cluster.key"))
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve dqlite cluster key: %w", err)
+		}
+		voters := make([]string, 0, len(dqliteCluster))
+		for _, node := range dqliteCluster {
+			if node.NodeRole == 0 {
+				voters = append(voters, node.Address)
+			}
+		}
+		response.DqliteVoterNodes = voters
+	}
+
+	return response, nil
+}
---
+++ pkg/api/v2/join_test.go
@@ -0,0 +1,315 @@
+package v2_test
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	v2 "github.com/canonical/microk8s-cluster-agent/pkg/api/v2"
+	"github.com/canonical/microk8s-cluster-agent/pkg/util"
+	utiltest "github.com/canonical/microk8s-cluster-agent/pkg/util/test"
+)
+
+// TestJoin tests responses when joining control plane and worker nodes in an existing cluster.
+func TestJoin(t *testing.T) {
+	apiv2 := &v2.API{
+		ListControlPlaneNodeIPs: mockListControlPlaneNodes("10.0.0.1", "10.0.0.2"),
+	}
+	// Create test data
+	for file, contents := range map[string]string{
+		"testdata/var/lock/ha-cluster":                "",
+		"testdata/var/kubernetes/backend/cluster.crt": "DQLITE CERTIFICATE DATA",
+		"testdata/var/kubernetes/backend/cluster.key": "DQLITE KEY DATA",
+		"testdata/var/kubernetes/backend/info.yaml": `
+Address: 10.10.10.10:19001
+ID: 1238719276943521
+Role: 0
+`,
+		"testdata/var/kubernetes/backend/cluster.yaml": `
+- Address: 10.10.10.10:19001
+  ID: 1238719276943521
+  Role: 0
+- Address: 10.10.10.11:19001
+  ID: 12312648746587658
+  Role: 0
+- Address: 10.10.10.100:19001
+  ID: 12312648746587655
+  Role: 2
+`,
+		"testdata/certs/ca.crt":                    "CA CERTIFICATE DATA",
+		"testdata/certs/ca.key":                    "CA KEY DATA",
+		"testdata/certs/serviceaccount.key":        "SERVICE ACCOUNT KEY DATA",
+		"testdata/args/kubelet":                    "kubelet arguments\n",
+		"testdata/args/kube-apiserver":             "--secure-port 16443\n--authorization-mode=Node,RBAC",
+		"testdata/args/cni-network/cni.yaml":       `some random content. "first-found"`,
+		"testdata/args/cluster-agent":              "--bind=0.0.0.0:25000",
+		"testdata/credentials/cluster-tokens.txt":  "worker-token\ncontrol-plane-token",
+		"testdata/credentials/callback-tokens.txt": "",
+		"testdata/credentials/callback-token.txt":  "callback-token",
+		"testdata/credentials/known_tokens.csv": `kube-proxy-token,system:kube-proxy,kube-proxy,
+admin-token-123,admin,admin,"system:masters"
+`,
+	} {
+		if err := os.MkdirAll(filepath.Dir(file), 0755); err != nil {
+			t.Fatalf("Failed to create test directory: %s", err)
+		}
+		if err := os.WriteFile(file, []byte(contents), 0660); err != nil {
+			t.Fatalf("Failed to create test file: %s", err)
+		}
+		defer os.RemoveAll(filepath.Dir(file))
+	}
+	defer os.RemoveAll("testdata/var")
+
+	t.Run("InvalidToken", func(t *testing.T) {
+		resp, err := apiv2.Join(context.Background(), v2.JoinRequest{ClusterToken: "invalid-token"})
+		if err == nil {
+			t.Fatalf("Expected error but did not receive any")
+		}
+		if resp != nil {
+			t.Fatalf("Expected a nil response but received %#v", resp)
+		}
+	})
+
+	t.Run("ControlPlane", func(t *testing.T) {
+		m := &utiltest.MockRunner{}
+		utiltest.WithMockRunner(m, func(t *testing.T) {
+
+			resp, err := apiv2.Join(context.Background(), v2.JoinRequest{
+				ClusterToken:     "control-plane-token",
+				RemoteHostName:   "some-invalid-hostname",
+				ClusterAgentPort: "25000",
+				HostPort:         "10.10.10.10:25000",
+				RemoteAddress:    "10.10.10.13:41532",
+				WorkerOnly:       false,
+			})
+			if err != nil {
+				t.Fatalf("Expected no errors, but received %q", err)
+			}
+			if resp == nil {
+				t.Fatal("Expected a response but received nil instead")
+			}
+
+			expectedResponse := &v2.JoinResponse{
+				CertificateAuthority:       "CA CERTIFICATE DATA",
+				CallbackToken:              "callback-token",
+				APIServerPort:              "16443",
+				APIServerAuthorizationMode: "Node,RBAC",
+				KubeletArgs:                "kubelet arguments\n\n--hostname-override=10.10.10.13",
+				HostNameOverride:           "10.10.10.13",
+				DqliteVoterNodes:           []string{"10.10.10.10:19001", "10.10.10.11:19001"},
+				ServiceAccountKey:          "SERVICE ACCOUNT KEY DATA",
+				CertificateAuthorityKey:    func(s string) *string { return &s }("CA KEY DATA"),
+				AdminToken:                 "admin-token-123",
+				DqliteClusterCertificate:   "DQLITE CERTIFICATE DATA",
+				DqliteClusterKey:           "DQLITE KEY DATA",
+			}
+			if !reflect.DeepEqual(*resp, *expectedResponse) {
+				t.Fatalf("Expected response %#v, but received %#v instead", expectedResponse, resp)
+			}
+			if util.IsValidClusterToken("control-plane-token") {
+				t.Fatalf("Expected control-plane-token to not be a valid cluster token after being used, but it is")
+			}
+
+			expectedCommands := []string{
+				"testdata/microk8s-kubectl.wrapper apply -f testdata/args/cni-network/cni.yaml",
+			}
+			if !reflect.DeepEqual(expectedCommands, m.CalledWithCommand) {
+				t.Fatalf("Expected commands %#v, but %#v was executed", expectedCommands, m.CalledWithCommand)
+			}
+			if !util.HasNoCertsReissueLock() {
+				t.Fatal("Expected certificate reissue lock to be in place after successful join, but it is not")
+			}
+		})(t)
+	})
+
+	t.Run("Worker", func(t *testing.T) {
+		m := &utiltest.MockRunner{}
+		utiltest.WithMockRunner(m, func(t *testing.T) {
+			resp, err := apiv2.Join(context.Background(), v2.JoinRequest{
+				ClusterToken:     "worker-token",
+				RemoteHostName:   "10.10.10.12",
+				RemoteAddress:    "10.10.10.12:31451",
+				WorkerOnly:       true,
+				HostPort:         "10.10.10.10:25000",
+				ClusterAgentPort: "25000",
+			})
+			if err != nil {
+				t.Fatalf("Expected no errors, but received %q", err)
+			}
+			if resp == nil {
+				t.Fatal("Expected a response but received nil instead")
+			}
+			expectedResponse := &v2.JoinResponse{
+				CertificateAuthority:       "CA CERTIFICATE DATA",
+				CallbackToken:              "callback-token",
+				APIServerAuthorizationMode: "Node,RBAC",
+				APIServerPort:              "16443",
+				KubeletArgs:                "kubelet arguments\n",
+				HostNameOverride:           "10.10.10.12",
+				ControlPlaneNodes:          []string{"10.0.0.1", "10.0.0.2"},
+			}
+
+			if !reflect.DeepEqual(*resp, *expectedResponse) {
+				t.Fatalf("Expected response %#v, but received %#v instead", expectedResponse, resp)
+			}
+			if util.IsValidClusterToken("worker-token") {
+				t.Fatalf("Expected worker-token to not be a valid cluster token after being used, but it is")
+			}
+			if !util.IsValidCertificateRequestToken("worker-token-kubelet") {
+				t.Fatal("Expected worker-token-kubelet to be a valid certificate request token, but it is not")
+			}
+			if !util.IsValidCertificateRequestToken("worker-token-proxy") {
+				t.Fatal("Expected worker-token-proxy to be a valid certificate request token, but it is not")
+			}
+			expectedCommands := []string{
+				"testdata/microk8s-kubectl.wrapper apply -f testdata/args/cni-network/cni.yaml",
+			}
+			if !reflect.DeepEqual(expectedCommands, m.CalledWithCommand) {
+				t.Fatalf("Expected commands %#v, but %#v was executed", expectedCommands, m.CalledWithCommand)
+			}
+			if !util.HasNoCertsReissueLock() {
+				t.Fatal("Expected certificate reissue lock to be in place after successful join, but it is not")
+			}
+		})(t)
+	})
+}
+
+// TestJoinFirstNode tests responses when joining a control plane node on a new cluster.
+// TestJoinFirstNode mocks the dqlite bind address update and verifies that is is handled properly.
+func TestJoinFirstNode(t *testing.T) {
+	apiv2 := &v2.API{}
+	m := &utiltest.MockRunner{}
+	utiltest.WithMockRunner(m, func(t *testing.T) {
+		// Create test data
+		for file, contents := range map[string]string{
+			"testdata/var/lock/ha-cluster":                "",
+			"testdata/var/kubernetes/backend/cluster.crt": "DQLITE CERTIFICATE DATA",
+			"testdata/var/kubernetes/backend/cluster.key": "DQLITE KEY DATA",
+			"testdata/var/kubernetes/backend/info.yaml": `
+Address: 127.0.0.1:19001
+ID: 1238719276943521
+Role: 0
+`,
+			"testdata/var/kubernetes/backend/cluster.yaml": `
+- Address: 127.0.0.1:19001
+  ID: 1238719276943521
+  Role: 0
+`,
+			"testdata/certs/ca.crt":                    "CA CERTIFICATE DATA",
+			"testdata/certs/ca.key":                    "CA KEY DATA",
+			"testdata/certs/serviceaccount.key":        "SERVICE ACCOUNT KEY DATA",
+			"testdata/args/kubelet":                    "kubelet arguments\n",
+			"testdata/args/kube-apiserver":             "--secure-port 16443\n--authorization-mode=Node",
+			"testdata/args/cluster-agent":              "--bind=0.0.0.0:25000",
+			"testdata/args/cni-network/cni.yaml":       `some content here. "first-found"`,
+			"testdata/credentials/cluster-tokens.txt":  "control-plane-token\n",
+			"testdata/credentials/callback-tokens.txt": "",
+			"testdata/credentials/callback-token.txt":  "callback-token",
+			"testdata/credentials/known_tokens.csv": `kube-proxy-token,system:kube-proxy,kube-proxy,
+admin-token-123,admin,admin,"system:masters"
+`,
+		} {
+			if err := os.MkdirAll(filepath.Dir(file), 0755); err != nil {
+				t.Fatalf("Failed to create test directory: %s", err)
+			}
+			if err := os.WriteFile(file, []byte(contents), 0660); err != nil {
+				t.Fatalf("Failed to create test file: %s", err)
+			}
+			defer os.RemoveAll(filepath.Dir(file))
+		}
+		defer os.RemoveAll("testdata/var")
+
+		go func() {
+			// update cluster with new address after a second
+			<-time.After(time.Second)
+			newCluster := `
+- Address: 10.10.10.10:19001
+  ID: 1238719276943521
+  Role: 0`
+			if err := os.WriteFile("testdata/var/kubernetes/backend/cluster.yaml", []byte(newCluster), 0660); err != nil {
+				panic(err)
+			}
+		}()
+
+		resp, err := apiv2.Join(context.Background(), v2.JoinRequest{
+			ClusterToken:     "control-plane-token",
+			RemoteHostName:   "some-invalid-hostname",
+			ClusterAgentPort: "25000",
+			HostPort:         "10.10.10.10:25000",
+			RemoteAddress:    "10.10.10.13:41532",
+			WorkerOnly:       false,
+		})
+		if err != nil {
+			t.Fatalf("Expected no errors, but received %q", err)
+		}
+		if resp == nil {
+			t.Fatal("Expected a response but received nil instead")
+		}
+
+		expectedResponse := &v2.JoinResponse{
+			CertificateAuthority:       "CA CERTIFICATE DATA",
+			CallbackToken:              "callback-token",
+			APIServerPort:              "16443",
+			APIServerAuthorizationMode: "Node",
+			KubeletArgs:                "kubelet arguments\n\n--hostname-override=10.10.10.13",
+			HostNameOverride:           "10.10.10.13",
+			DqliteVoterNodes:           []string{"10.10.10.10:19001"},
+			ServiceAccountKey:          "SERVICE ACCOUNT KEY DATA",
+			CertificateAuthorityKey:    func(s string) *string { return &s }("CA KEY DATA"),
+			AdminToken:                 "admin-token-123",
+			DqliteClusterCertificate:   "DQLITE CERTIFICATE DATA",
+			DqliteClusterKey:           "DQLITE KEY DATA",
+		}
+		if !reflect.DeepEqual(*resp, *expectedResponse) {
+			t.Fatalf("Expected response %#v, but received %#v instead", expectedResponse, resp)
+		}
+		if util.IsValidClusterToken("control-plane-token") {
+			t.Fatalf("Expected control-plane-token to not be a valid cluster token after being used, but it is")
+		}
+		s, err := util.ReadFile("testdata/var/kubernetes/backend/update.yaml")
+		if err != nil {
+			t.Fatalf("Failed to read dqlite update yaml file: %q", err)
+		} else if strings.TrimSpace(s) != "Address: 10.10.10.10:19001" {
+			t.Fatalf("Expected dqlite update address does not match (%q and %q)", strings.TrimSpace(s), "Address: 10.10.10.10.19001")
+		}
+
+		expectedCommands := []string{
+			"snapctl restart microk8s.daemon-k8s-dqlite",
+			"testdata/microk8s-kubectl.wrapper apply -f testdata/args/cni-network/cni.yaml",
+		}
+		if !reflect.DeepEqual(expectedCommands, m.CalledWithCommand) {
+			t.Fatalf("Expected commands %#v to be called, but received %#v", expectedCommands, m.CalledWithCommand)
+		}
+		if !util.HasNoCertsReissueLock() {
+			t.Fatal("Expected certificate reissue lock to be in place after successful join, but it is not")
+		}
+	})(t)
+}
+
+func TestUnmarshalWorkerOnlyField(t *testing.T) {
+	for _, tc := range []struct {
+		b             string
+		expectedValue v2.WorkerOnlyField
+	}{
+		{b: "true", expectedValue: true},
+		{b: "false", expectedValue: false},
+		{b: "null", expectedValue: false},
+		{b: `"as-worker"`, expectedValue: true},
+		{b: `"as-controlplane"`, expectedValue: false},
+	} {
+		t.Run(tc.b, func(t *testing.T) {
+			var v v2.WorkerOnlyField
+			if err := json.Unmarshal([]byte(tc.b), &v); err != nil {
+				t.Fatalf("Expected no error but received %q", err)
+			}
+			if v != tc.expectedValue {
+				t.Fatalf("Expected value to be %v, but it was %v", tc.expectedValue, v)
+			}
+		})
+	}
+}
