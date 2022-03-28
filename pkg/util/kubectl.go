984ccd05588de3cb0e8f35c96c0435dad69f891e
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-03-28 15:46:20 +0300

    fixup! Use k8s.io/client-go

--- pkg/util/kubectl.go
+++ pkg/util/kubectl.go
@@ -2,7 +2,6 @@ package util
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 
 	v1 "k8s.io/api/core/v1"
@@ -11,34 +10,17 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// kubectlGetNodesJSON parses the output of the "kubectl get nodes -o json" command.
-type kubectlGetNodesJSON struct {
-	Items []struct {
-		Status struct {
-			Addresses []struct {
-				Address string `json:"address"`
-				Type    string `json:"type"`
-			} `json:"addresses"`
-		} `json:"status"`
-	} `json:"items"`
-}
-
-func parseControlPlaneNodeIPs(jsonOutput []byte) ([]string, error) {
-	var response kubectlGetNodesJSON
-	if err := json.Unmarshal(jsonOutput, &response); err != nil {
-		return nil, fmt.Errorf("failed to parse kubectl command output: %w", err)
-	}
-
-	nodes := make([]string, 0, len(response.Items))
-	for _, item := range response.Items {
-		for _, address := range item.Status.Addresses {
-			if address.Type == "InternalIP" {
-				nodes = append(nodes, address.Address)
+func parseNodeInternalIPs(nodeList []v1.Node) []string {
+	addresses := make([]string, 0, len(nodeList))
+	for _, node := range nodeList {
+		for _, address := range node.Status.Addresses {
+			if address.Type == v1.NodeInternalIP {
+				addresses = append(addresses, address.Address)
 			}
 		}
 	}
 
-	return nodes, nil
+	return addresses
 }
 
 // ListControlPlaneNodeIPs returns the internal IPs of the control plane nodes of the MicroK8s cluster.
@@ -59,14 +41,5 @@ func ListControlPlaneNodeIPs(ctx context.Context) ([]string, error) {
 		return nil, fmt.Errorf("failed to list nodes: %w", err)
 	}
 
-	addresses := make([]string, 0, len(nodes.Items))
-	for _, node := range nodes.Items {
-		for _, address := range node.Status.Addresses {
-			if address.Type == v1.NodeInternalIP {
-				addresses = append(addresses, address.Address)
-			}
-		}
-	}
-
-	return addresses, nil
+	return parseNodeInternalIPs(nodes.Items), nil
 }
--- pkg/util/kubectl_test.go
+++ pkg/util/kubectl_test.go
@@ -3,22 +3,33 @@ package util
 import (
 	"reflect"
 	"testing"
+
+	v1 "k8s.io/api/core/v1"
 )
 
-func TestParseControlPlaneNodeIPs(t *testing.T) {
-	b := []byte(`{
-		"items": [
-			{"status": {"addresses": [{"type": "InternalIP", "address": "10.0.0.1"}, {"type": "Hostname", "address": "node1"}]}},
-			{"status": {"addresses": [{"type": "InternalIP", "address": "10.0.0.2"}, {"type": "Hostname", "address": "node2"}]}}
-		]
-	}`)
+func TestParseNodeInternalIPs(t *testing.T) {
+	nodeList := []v1.Node{
+		{
+			Status: v1.NodeStatus{
+				Addresses: []v1.NodeAddress{
+					{Type: v1.NodeHostName, Address: "host1"},
+					{Type: v1.NodeInternalIP, Address: "10.0.0.1"},
+				},
+			},
+		},
+		{
+			Status: v1.NodeStatus{
+				Addresses: []v1.NodeAddress{
+					{Type: v1.NodeHostName, Address: "host2"},
+					{Type: v1.NodeInternalIP, Address: "10.0.0.2"},
+				},
+			},
+		},
+	}
 	expectedIPs := []string{"10.0.0.1", "10.0.0.2"}
 
-	nodes, err := parseControlPlaneNodeIPs(b)
-	if err != nil {
-		t.Fatalf("expected no error but got %q", err)
-	}
-	if !reflect.DeepEqual(nodes, expectedIPs) {
-		t.Fatalf("expected list of nodes to be %v but it was %v instead", expectedIPs, nodes)
+	ips := parseNodeInternalIPs(nodeList)
+	if !reflect.DeepEqual(ips, expectedIPs) {
+		t.Fatalf("expected list of nodes to be %v but it was %v instead", expectedIPs, ips)
 	}
 }
