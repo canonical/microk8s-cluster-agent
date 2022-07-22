487f02777dad17deb652cf6462abfd7943842fdb
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-07-23 01:50:40 +0300

    test endpoint parsing

--- pkg/proxy/endpoints.go
+++ pkg/proxy/endpoints.go
@@ -5,24 +5,15 @@ import (
 	"fmt"
 	"sort"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func getKubernetesEndpoints(ctx context.Context, kubeconfigFile string) ([]string, error) {
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigFile)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read load kubeconfig: %w", err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize kubernetes client: %w", err)
-	}
-
-	endpoint, err := clientset.CoreV1().Endpoints("default").Get(ctx, "kubernetes", metav1.GetOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve endpoints for kubernetes service: %w", err)
+func parseAddresses(endpoint *v1.Endpoints) []string {
+	if endpoint == nil {
+		return nil
 	}
 	addresses := make([]string, 0, len(endpoint.Subsets))
 	for _, subset := range endpoint.Subsets {
@@ -40,6 +31,22 @@ func getKubernetesEndpoints(ctx context.Context, kubeconfigFile string) ([]strin
 	}
 
 	sort.Strings(addresses)
+	return addresses
+}
 
-	return addresses, nil
+func getKubernetesEndpoints(ctx context.Context, kubeconfigFile string) ([]string, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read load kubeconfig: %w", err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize kubernetes client: %w", err)
+	}
+
+	endpoint, err := clientset.CoreV1().Endpoints("default").Get(ctx, "kubernetes", metav1.GetOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve endpoints for kubernetes service: %w", err)
+	}
+	return parseAddresses(endpoint), nil
 }
