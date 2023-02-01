45c46943eb7f685372a599d2c8abfafb2c62d52e
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-02-01 17:45:40 +0200

    reconcile kubelet args

--- pkg/k8sinit/apply.go
+++ pkg/k8sinit/apply.go
@@ -3,6 +3,8 @@ package k8sinit
 import (
 	"context"
 	"fmt"
+
+	snaputil "github.com/canonical/microk8s-cluster-agent/pkg/snap/util"
 )
 
 // Apply applies a multi-part configuration to the local MicroK8s node.
@@ -25,6 +27,8 @@ func (l *Launcher) applyPart(ctx context.Context, c *Configuration) error {
 		l.reconcileAddons(ctx, c.Addons)
 	}
 
+	if err := l.reconcileKubeletArgs(ctx, c.ExtraKubeletArgs); err != nil {
+		return fmt.Errorf("failed to configure extra kubelet args: %w", err)
 	}
 
 	return nil
@@ -40,5 +44,35 @@ func (l *Launcher) reconcileAddons(ctx context.Context, addons []AddonConfigurat
 			return fmt.Errorf("failed to enable addon %q: %w", addon.Name, err)
 		}
 	}
+	return nil
+}
+
+func (l *Launcher) reconcileKubeletArgs(ctx context.Context, args map[string]*string) error {
+	if len(args) == 0 {
+		return nil
+	}
+	updateArgs := map[string]string{}
+	deleteArgs := []string{}
+
+	for key, valptr := range args {
+		if valptr == nil {
+			deleteArgs = append(deleteArgs, key)
+		} else {
+			updateArgs[key] = *valptr
+		}
+	}
+
+	changed, err := snaputil.UpdateServiceArguments(l.snap, "kubelet", []map[string]string{updateArgs}, deleteArgs)
+	if err != nil {
+		return fmt.Errorf("failed to update service arguments: %w", err)
+	}
+
+	// TODO(neoaggelos): restart services should be deferred until the very end of the function
+	if changed && !l.preInit {
+		if err := l.snap.RestartService(ctx, "kubelet"); err != nil {
+			return fmt.Errorf("failed to restart kubelet service after updating arguments: %w", err)
+		}
+	}
+
 	return nil
 }
--- pkg/k8sinit/schema.go
+++ pkg/k8sinit/schema.go
@@ -42,6 +42,10 @@ type Configuration struct {
 
 	// Addons is a list of addons to enable and/or disable.
 	Addons []AddonConfiguration `yaml:"addons"`
+
+	// ExtraKubeletArgs is a list of extra arguments to add to the local node kubelet.
+	// Set a value to null to remove it from the arguments.
+	ExtraKubeletArgs map[string]*string `yaml:"extraKubeletArgs"`
 }
 
 // ParseConfiguration tries to parse a Configuration object from YAML data.
