2508a93288343e1d0e66f1ae768664fab8b3440b
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-04-06 15:54:23 +0300

    Add --min-tls-version flag (#30)

--- cmd/cluster-agent.go
+++ cmd/cluster-agent.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"crypto/tls"
 	"log"
 	"net"
 	"net/http"
@@ -21,6 +22,7 @@ var (
 	certfile      string
 	timeout       int
 	enableMetrics bool
+	minTLSVersion string
 )
 
 // clusterAgentCmd represents the base command when called without any subcommands
@@ -46,9 +48,35 @@ lifecycle of a MicroK8s cluster.`,
 			InterfaceAddrs:          net.InterfaceAddrs,
 			ListControlPlaneNodeIPs: snaputil.ListControlPlaneNodeIPs,
 		}
-		srv := server.NewServer(time.Duration(timeout)*time.Second, enableMetrics, apiv1, apiv2)
+		mux := server.NewServeMux(time.Duration(timeout)*time.Second, enableMetrics, apiv1, apiv2)
+		srv := &http.Server{
+			Addr:    bind,
+			Handler: mux,
+		}
+
+		switch minTLSVersion {
+		case "tls10":
+			srv.TLSConfig = &tls.Config{
+				MinVersion: tls.VersionTLS10,
+			}
+		case "tls11":
+			srv.TLSConfig = &tls.Config{
+				MinVersion: tls.VersionTLS11,
+			}
+		case "", "tls12":
+			srv.TLSConfig = &tls.Config{
+				MinVersion: tls.VersionTLS12,
+			}
+		case "tls13":
+			srv.TLSConfig = &tls.Config{
+				MinVersion: tls.VersionTLS13,
+			}
+		default:
+			log.Printf("ERROR: Unsupported TLS version %v. Supported values: tls10, tls11, tls12, tls13.", minTLSVersion)
+		}
+
 		log.Printf("Starting cluster agent on https://%s\n", bind)
-		if err := http.ListenAndServeTLS(bind, certfile, keyfile, srv); err != nil {
+		if err := srv.ListenAndServeTLS(certfile, keyfile); err != nil {
 			log.Fatalf("Failed to listen: %s", err)
 		}
 	},
@@ -60,6 +88,7 @@ func init() {
 	clusterAgentCmd.Flags().StringVar(&certfile, "certfile", "", "Certificate for serving TLS")
 	clusterAgentCmd.Flags().IntVar(&timeout, "timeout", 240, "Default request timeout (in seconds)")
 	clusterAgentCmd.Flags().BoolVar(&enableMetrics, "enable-metrics", false, "Enable metrics endpoint")
+	clusterAgentCmd.Flags().StringVar(&minTLSVersion, "min-tls-version", "tls12", "Minimum TLS version required (tls10|tls11|tls12|tls13). Default is tls12")
 
 	rootCmd.AddCommand(clusterAgentCmd)
 }
