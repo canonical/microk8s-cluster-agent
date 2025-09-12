71cdb78839e5eb03bce750bb72efa9e147f4c88d
Berkay Tekin Öz <berkay.tekinoz@canonical.com>
2025-09-12 12:12:49 +0300

    fix: fallback to endpoints if endpointslice unavailable (#71)

--- pkg/proxy/endpoints.go
+++ pkg/proxy/endpoints.go
@@ -1,77 +1,33 @@
 package proxy
 
+//lint:file-ignore SA1019 Ignore usage of deprecated k8s api
+
 import (
-	"context"
 	"fmt"
 	"sort"
 
-	discoveryv1 "k8s.io/api/discovery/v1"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	v1 "k8s.io/api/core/v1"
 )
 
-func parseAddresses(endpointSlices *discoveryv1.EndpointSliceList) []string {
-	if endpointSlices == nil {
+func parseEndpoints(endpoint *v1.Endpoints) []string {
+	if endpoint == nil {
 		return nil
 	}
-
-	addresses := make([]string, 0, len(endpointSlices.Items))
-
-	for _, endpointSlice := range endpointSlices.Items {
+	addresses := make([]string, 0, len(endpoint.Subsets))
+	for _, subset := range endpoint.Subsets {
 		portNumber := 16443
-		for _, port := range endpointSlice.Ports {
-			if port.Name != nil && *port.Name == "https" {
-				if port.Port != nil {
-					portNumber = int(*port.Port)
-					break
-				}
+		for _, port := range subset.Ports {
+			if port.Name == "https" {
+				portNumber = int(port.Port)
+				break
 			}
 		}
 
-		for _, endpoint := range endpointSlice.Endpoints {
-			for _, addr := range endpoint.Addresses {
-				if addr != "" {
-					var address string
-					switch endpointSlice.AddressType {
-					case discoveryv1.AddressTypeIPv4:
-						address = addr
-					case discoveryv1.AddressTypeIPv6:
-						address = fmt.Sprintf("[%s]", addr)
-					case discoveryv1.AddressTypeFQDN:
-						// Not supported, skip.
-						continue
-					default:
-						// Unknown address type, skip.
-						continue
-					}
-					addresses = append(addresses, fmt.Sprintf("%s:%d", address, portNumber))
-				}
-			}
+		for _, addr := range subset.Addresses {
+			addresses = append(addresses, fmt.Sprintf("%s:%d", addr.IP, portNumber))
 		}
-
 	}
 
 	sort.Strings(addresses)
 	return addresses
-}
-
-func getKubernetesEndpoints(ctx context.Context, kubeconfigFile string) ([]string, error) {
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigFile)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read load kubeconfig: %w", err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize kubernetes client: %w", err)
-	}
-
-	endpointSlices, err := clientset.DiscoveryV1().EndpointSlices("default").List(ctx, metav1.ListOptions{
-		LabelSelector: "kubernetes.io/service-name=kubernetes",
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve endpoints for kubernetes service: %w", err)
-	}
-	return parseAddresses(endpointSlices), nil
 }
---
+++ pkg/proxy/endpointslices_test.go
@@ -0,0 +1,168 @@
+package proxy
+
+import (
+	"reflect"
+	"testing"
+
+	discoveryv1 "k8s.io/api/discovery/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+)
+
+func TestParseEndpointSlices(t *testing.T) {
+	httpsPortName := "https"
+	for _, tc := range []struct {
+		name           string
+		endpointSlices *discoveryv1.EndpointSliceList
+		addresses      []string
+	}{
+		{
+			name: "one",
+			endpointSlices: &discoveryv1.EndpointSliceList{
+				Items: []discoveryv1.EndpointSlice{
+					{
+						ObjectMeta:  metav1.ObjectMeta{Name: "test-1"},
+						AddressType: discoveryv1.AddressTypeIPv4,
+						Endpoints: []discoveryv1.Endpoint{
+							{Addresses: []string{"1.1.1.1"}},
+						},
+						Ports: []discoveryv1.EndpointPort{
+							{Name: &httpsPortName, Port: ptr.To(int32(16643))},
+						},
+					},
+				},
+			},
+			addresses: []string{"1.1.1.1:16643"},
+		},
+		{
+			name: "two",
+			endpointSlices: &discoveryv1.EndpointSliceList{
+				Items: []discoveryv1.EndpointSlice{
+					{
+						ObjectMeta:  metav1.ObjectMeta{Name: "test-2"},
+						AddressType: discoveryv1.AddressTypeIPv4,
+						Endpoints: []discoveryv1.Endpoint{
+							{Addresses: []string{"1.1.1.1"}},
+							{Addresses: []string{"2.2.2.2"}},
+						},
+						Ports: []discoveryv1.EndpointPort{
+							{Name: &httpsPortName, Port: ptr.To(int32(16643))},
+						},
+					},
+				},
+			},
+			addresses: []string{"1.1.1.1:16643", "2.2.2.2:16643"},
+		},
+		{
+			name: "IPv6",
+			endpointSlices: &discoveryv1.EndpointSliceList{
+				Items: []discoveryv1.EndpointSlice{
+					{
+						ObjectMeta:  metav1.ObjectMeta{Name: "test-ipv6"},
+						AddressType: discoveryv1.AddressTypeIPv6,
+						Endpoints: []discoveryv1.Endpoint{
+							{Addresses: []string{"fe80::e0b9:bfff:fe90:8d37"}},
+						},
+						Ports: []discoveryv1.EndpointPort{
+							{Name: &httpsPortName, Port: ptr.To(int32(16643))},
+						},
+					},
+				},
+			},
+			addresses: []string{"[fe80::e0b9:bfff:fe90:8d37]:16643"},
+		},
+		{
+			name: "multiple-slices",
+			endpointSlices: &discoveryv1.EndpointSliceList{
+				Items: []discoveryv1.EndpointSlice{
+					{
+						ObjectMeta:  metav1.ObjectMeta{Name: "test-multi-1"},
+						AddressType: discoveryv1.AddressTypeIPv4,
+						Endpoints: []discoveryv1.Endpoint{
+							{Addresses: []string{"1.1.1.1"}},
+							{Addresses: []string{"2.2.2.2"}},
+						},
+						Ports: []discoveryv1.EndpointPort{
+							{Name: &httpsPortName, Port: ptr.To(int32(16643))},
+						},
+					},
+					{
+						ObjectMeta:  metav1.ObjectMeta{Name: "test-multi-2"},
+						AddressType: discoveryv1.AddressTypeIPv4,
+						Endpoints: []discoveryv1.Endpoint{
+							{Addresses: []string{"3.3.3.3"}},
+						},
+						Ports: []discoveryv1.EndpointPort{
+							{Name: &httpsPortName, Port: ptr.To(int32(16643))},
+						},
+					},
+				},
+			},
+			addresses: []string{"1.1.1.1:16643", "2.2.2.2:16643", "3.3.3.3:16643"},
+		},
+		{
+			name: "override-port",
+			endpointSlices: &discoveryv1.EndpointSliceList{
+				Items: []discoveryv1.EndpointSlice{
+					{
+						ObjectMeta:  metav1.ObjectMeta{Name: "test-port-1"},
+						AddressType: discoveryv1.AddressTypeIPv4,
+						Endpoints: []discoveryv1.Endpoint{
+							{Addresses: []string{"1.1.1.1"}},
+							{Addresses: []string{"2.2.2.2"}},
+						},
+						Ports: []discoveryv1.EndpointPort{
+							{Name: &httpsPortName, Port: ptr.To(int32(16643))},
+						},
+					},
+					{
+						ObjectMeta:  metav1.ObjectMeta{Name: "test-port-2"},
+						AddressType: discoveryv1.AddressTypeIPv4,
+						Endpoints: []discoveryv1.Endpoint{
+							{Addresses: []string{"3.3.3.3"}},
+						},
+						Ports: []discoveryv1.EndpointPort{
+							{Name: &httpsPortName, Port: ptr.To(int32(10000))},
+						},
+					},
+				},
+			},
+			addresses: []string{"1.1.1.1:16643", "2.2.2.2:16643", "3.3.3.3:10000"},
+		},
+		{
+			name: "sort",
+			endpointSlices: &discoveryv1.EndpointSliceList{
+				Items: []discoveryv1.EndpointSlice{
+					{
+						ObjectMeta:  metav1.ObjectMeta{Name: "test-sort-1"},
+						AddressType: discoveryv1.AddressTypeIPv4,
+						Endpoints: []discoveryv1.Endpoint{
+							{Addresses: []string{"3.3.3.3"}},
+							{Addresses: []string{"1.1.1.1"}},
+						},
+						Ports: []discoveryv1.EndpointPort{
+							{Name: &httpsPortName, Port: ptr.To(int32(16643))},
+						},
+					},
+					{
+						ObjectMeta:  metav1.ObjectMeta{Name: "test-sort-2"},
+						AddressType: discoveryv1.AddressTypeIPv4,
+						Endpoints: []discoveryv1.Endpoint{
+							{Addresses: []string{"2.2.2.2"}},
+						},
+						Ports: []discoveryv1.EndpointPort{
+							{Name: &httpsPortName, Port: ptr.To(int32(10000))},
+						},
+					},
+				},
+			},
+			addresses: []string{"1.1.1.1:16643", "2.2.2.2:10000", "3.3.3.3:16643"},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			if parsed := parseEndpointSlices(tc.endpointSlices); !reflect.DeepEqual(parsed, tc.addresses) {
+				t.Fatalf("expected addresses to be %v but they were %v instead", tc.addresses, parsed)
+			}
+		})
+	}
+}
